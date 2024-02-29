7baa018c64abfd13c8e49d21a5646242c707bb4b
frhtfettah <fettahogluferhat@gmail.com>
2024-02-29 09:27:12 +0300

    rest methods

---
+++ src/test/java/src/Base.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Base {
+
+   static String baseUri = "https://httpbin.org";
+
+}
---
+++ src/test/java/src/Body.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Body {
+
+    int statusCodeOK = 200;
+    String apiResponseCheckFirst = "Why <em>WonderWidgets</em> are great";
+    String apiResponseCheckSecond = "Who <em>buys</em> WonderWidgets";
+
+}
